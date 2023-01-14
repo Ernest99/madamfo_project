ada466c26f15bd61a8019c992b97a24b7581409b
vibrant_coder <74645353+Ernest99@users.noreply.github.com>
2023-01-14 01:52:08 +0000

    added swiperjs

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ menu_bar.onclick = () =>{
     humburger.classList.toggle('bi-x');
     nav_menu.classList.toggle('show_menu');
 }
+
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "flip",
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
