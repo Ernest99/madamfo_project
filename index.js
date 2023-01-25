b16a58ec00261cb4611c505a092990652f4747d9
vibrant_coder <74645353+Ernest99@users.noreply.github.com>
2023-01-25 21:12:22 +0000

    fixed bugs

--- index.js
+++ index.js
@@ -12,14 +12,7 @@ var swiper = new Swiper(".mySwiper", {
 
 
 
-let menu_bar = document.getElementById('menu_icon');
-let humburger = document.getElementById('hum_menu');
-let nav_menu = document.getElementById('nav_link');
 
-menu_bar.onclick = () =>{
-    humburger.classList.toggle('bi-x');
-    nav_menu.classList.toggle('show_menu');
-}
 
 
 
